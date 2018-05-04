6619fb0256a9a09a7d3cee05a62a481f1f16c4b3
tijohask <tijohask@gmail.com>
2018-05-04 15:06:35 -0400

    Added a script that shows an example of all the array generation algorithms.

---
+++ example.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Example of covering array generation:"
+python3 Master_File.py  2  2,2,2,2,3,2,2,2  1
+echo ""
+python3 Master_File.py  2  2,2,2,2,3,2,2,2  2
+echo ""
+python3 Master_File.py  2  2,2,2,2,3,2,2,2  3
+echo ""
+python3 Master_File.py  2  2,2,2,2,3,2,2,2  4
+echo ""
+python3 Master_File.py  2  2,2,2,2,3,2,2,2  5
